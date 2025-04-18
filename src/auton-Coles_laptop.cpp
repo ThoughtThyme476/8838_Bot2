e3e2c9a5aab2c05db410dfeca66c5f392d971044
ThoughtThyme476 <cole.herro@icloud.com>
2025-04-17 19:53:32 -0700

    I reuploaded in case my OneDrive instal messed with something. Sorry about the folder names, theres literally nothing I can do. I tried rename, ctrl + z, etc. Didn't work

---
+++ src/auton-Coles_laptop.cpp
@@ -0,0 +1,214 @@
+#include "main.h"
+#include "api.h"
+#include "pid.h"
+#include "robot.h"
+#include "auton.h"
+ using namespace pros;
+
+//for(int i = 0; i<=800; i++){
+//ColorSort();
+//delay(1);
+//}
+
+// auton1=red side ring side
+// auton2=blue side ring side
+// auton3= red side elims goal side
+// autons4= blue side elims goal side
+// autons5 = auto skills
+
+ //don't use autons 1 and that stuff anymore just use the above 
+
+ void Standard_AWP_red(){
+  //1 = no blue rings, 2 = no red rings
+  eyes.set_led_pwm(100); //always do this
+  LadyBrown.move(-110);
+  driveStraight2(250);
+  LadyBrown.move(20);
+  driveStraightC(-400);
+  LadyBrown.move(90);
+  driveStraight2(-900);
+  driveTurn2(-7);
+  driveClampS(-850, 300, 100);
+  driveTurn2(160);
+  color = 1;
+  //color = 1;
+  driveStraight2(610);
+  //delay(200);
+  driveTurn2(60);
+  driveStraight2(550);
+  driveTurn2(-24);
+  driveStraightC(1250);
+  MogoMech.set_value(false);
+  Intake.move(127);
+  driveArcLF(40, 400, 500, 100);
+  driveStraight2(100);
+  //driveTurn2(-25);
+  color = 0;
+  Intake.move(127);
+  driveSortHoldRed(1100, 50);
+  Intake.move(0);
+  driveTurn2(50);
+  driveStraight2(-1100);
+  driveClampS(-550, 200, 60);
+  Intake.move(127);
+  driveTurn2(165);
+  LadyBrown.move(-90);
+ }
+ void Goal_Rush_red(){ // power beans goal rush kinda. Did this saturday night/sunday moring durning states 
+  // color = 1;//1 = no blue rings, 2 = no red rings
+  eyes.set_led_pwm(100);
+  StakeWing.set_value(true);
+  driveStraightC(1200);
+  driveArcRF(45, 370, 950, 100);
+  StakeWing.set_value(false);
+  driveStraight2(10);
+  driveStraightC(-500);
+  driveTurn2(0);
+  driveStraightC(-500);
+  StakeWing.set_value(true);
+  driveStraight2(-250);
+  driveTurn2(60);
+  driveSortHoldRed(1200, 80);
+
+
+ }
+ void Standard_AWP_blue(){
+  eyes.set_led_pwm(100); //always do this
+  LadyBrown.move(-110);
+  driveStraight2(250);
+  LadyBrown.move(20);
+  driveStraightC(-400);
+  LadyBrown.move(90);
+  driveStraight2(-900);
+  driveTurn2(7);
+  driveClampS(-850, 300, 100);
+  driveTurn2(-160);
+  color = 2;
+  //color = 1;
+  driveStraight2(590);
+  //delay(200);
+  driveTurn2(-60);
+  driveStraight2(500);
+  driveTurn2(25);
+  driveStraightC(1650);
+  MogoMech.set_value(false);
+  Intake.move(127);
+  driveArcRF(15, 400, 500, 100);
+  driveStraight2(100);
+  //driveTurn2(-25);
+  color = 0;
+  Intake.move(127);
+  driveSortHoldblue(1100, 50);
+  Intake.move(0);
+  driveTurn2(-45);
+  driveStraight2(-1100);
+  driveClampS(-550, 200, 60);
+  Intake.move(127);
+  driveTurn2(-165);
+  LadyBrown.move(-90);
+  }
+
+
+ void Goal_Rush_Blue(){ // need to fix color sensor and goal tip side because of no double doinkers!!
+  //blue goal rush
+  eyes.set_led_pwm(100);
+  Intake.move(127);
+  driveStraight2(1650);
+  Intake.move(0);
+  driveStraightSlow(850, 100);
+  StakeWing.set_value(true);
+  delay(100);
+  Intake_Piston.set_value(true);
+  driveTurn2(110);
+  StakeWing.set_value(false);
+  driveTurn2(-70);
+  driveStraight2(-600);
+  driveClampS(-800, 50, 50);
+  delay(100);
+  Intake.move(127);
+  driveStraightC(600);
+  driveTurn2(165);
+  Intake_Piston.set_value(false);
+  driveStraight2(2000);
+ // color = 1;
+  driveStraightSlow(300, 50);
+  delay(500);
+  driveStraight2(-500);
+  driveTurn2(45);
+  driveStraight2(-1000);
+ }
+
+//blue ring side touch bar bc not doing corner stack
+ void autons6(){
+ //blue ring side
+   LadyBrown.move(-17);
+   delay(50);
+   driveTurn2(39);
+   driveStraight2(360);
+   LadyBrown.move(-127);
+   delay(650);
+   LadyBrown.move(127);
+   delay(300);
+   LadyBrown.move(0);
+   driveStraight2(-1150);
+   driveTurn2(-11);
+   driveClampS(-1250, 100, 50);
+   driveStraight2(500);
+   driveTurn2(-90);
+   Intake.move(-90);
+   //ErrorFixayer1.move(-127);
+   driveStraight2(900);
+   delay(400);
+   driveTurn2(-180);
+   driveStraight2(470);
+   driveTurn2(-115);
+   driveStraight2(100);
+   delay(150);
+   driveTurn2(80);
+   driveStraight2(1111);
+  }
+ 
+ void skill_run(){
+  LadyBrown.move_relative(-900, 127);
+  delay(400);
+  driveStraight2(-500);
+  driveTurn2(90);
+  driveClampS(-1100, 50, 50);
+  driveTurn2(-90);
+  Intake.move(127);
+  LadyBrown.move_relative(400, 127);
+  driveStraight2(1800);
+  driveTurn2(-200);
+  MogoMech.set_value(true);
+  driveStraight2(-1000);
+  delay(100);
+  driveStraight2(500);
+ }
+ 
+ void autonomous(){
+  if (atn == 0) {
+  } else if (atn == 1) {
+    // color = 1;
+  Standard_AWP_red();
+  } 
+  else if (atn ==2) {
+  Standard_AWP_blue();
+  } 
+  else if (atn ==3) {
+    Goal_Rush_red();
+  } 
+  else if (atn ==4) {
+    Goal_Rush_Blue();
+  } 
+  else if (atn ==5) {
+    skill_run();
+  } 
+  else if (atn ==6) {
+
+  } 
+  else if (atn ==7) {
+    eyes.set_led_pwm(100);
+    Intake.move(127);
+    driveSortHoldRed(750, 25);
+  }
+}
---
+++ src/main-Coles_laptop.cpp
@@ -0,0 +1,365 @@
+
+#include "main.h"
+#include "api.h"
+#include "pid.h"
+#include "robot.h"
+#include "auton.h"
+bool stay_clamp = true;
+
+using namespace pros;
+using namespace std;
+
+int atn = 3;
+string autstr;
+// Task colorSortTask;
+// bool colorSortActive = false;
+
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}//bla bla bla
+//bla bla bla
+
+void initialize() {
+	// pros::lcd::initialize();
+	// pros::lcd::set_text(1, "Hello PROS User!");
+
+	// pros::lcd::register_btn1_cb(on_center_button);
+
+	// while(true){
+	// 	if (selec.get_value() == true){
+	// 		atn++;
+	//delay(350);
+	// 	}
+	// 	if(atn>7){//change number to number of autons
+	// 		atn=0;
+	// 	}
+	// 	if (atn == 0) {
+	// 		autstr = "NONE";
+	// 		con.print(0,0, "Aut 0:%s", autstr);
+	// 	}
+	// 	else if (atn ==1) {
+	// 		autstr = "RED RING";
+	// 		con.print(0,0, "Aut 1: %s", autstr);
+	// 	}
+	// 	else if (atn ==2) {
+	// 		autstr = "BLUE RING";
+	// 		con.print(0,0, "Aut 2: %s", autstr);
+	// 	}
+	// 	else if (atn ==3) {
+	// 		autstr = "RED GOAL QUALS";
+	// 		con.print(0,0, "Aut 3: %s", autstr);
+	// 	}
+	// 	else if (atn ==4) {
+	// 		autstr = "BLUE GOAL QUALS";
+	// 		con.print(0,0, "Aut 4: %s", autstr);
+	// 	}
+	// 	else if (atn ==5) {
+	// 		autstr = "RED GOAL ELIMS";
+	// 		con.print(0,0, "Aut 5: %s", autstr);
+	// 	}
+	// 	else if (atn ==6) {
+	// 		autstr = "BLUE GOALS ELIMS";
+	// 		con.print(0,0, "Aut 6: %s", autstr);
+	// 	}
+	// 	else if (atn ==7) {
+	// 		autstr = "SKILLS";
+	// 		con.print(0,0, "Aut 7: %s", autstr);
+	// 	}
+
+	// 	if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_X)){
+	// 		break;
+	// 	}
+
+
+
+
+	// 		}
+
+//auton selector
+
+}
+void disabled() { 
+	// imu.tare(); 
+	// while(stay_clamp = true){
+	// 	MogoMech.set_value(true);
+	// }
+}
+
+//void autonomous moved into just auton.cpp
+
+void competition_initialize() {
+// while(true){
+	// 	if (selec.get_value() == true){
+	// 		atn++;
+	//delay(350);
+	// 	}
+	// 	if(atn>7){//change number to number of autons
+	// 		atn=0;
+	// 	}
+	// 	if (atn == 0) {
+	// 		autstr = "NONE";
+	// 		con.print(0,0, "Aut 0:%s", autstr);
+	// 	}
+	// 	else if (atn ==1) {
+	// 		autstr = "RED RING";
+	// 		con.print(0,0, "Aut 1: %s", autstr);
+	// 	}
+	// 	else if (atn ==2) {
+	// 		autstr = "BLUE RING";
+	// 		con.print(0,0, "Aut 2: %s", autstr);
+	// 	}
+	// 	else if (atn ==3) {
+	// 		autstr = "RED GOAL QUALS";
+	// 		con.print(0,0, "Aut 3: %s", autstr);
+	// 	}
+	// 	else if (atn ==4) {
+	// 		autstr = "BLUE GOAL QUALS";
+	// 		con.print(0,0, "Aut 4: %s", autstr);
+	// 	}
+	// 	else if (atn ==5) {
+	// 		autstr = "RED GOAL ELIMS";
+	// 		con.print(0,0, "Aut 5: %s", autstr);
+	// 	}
+	// 	else if (atn ==6) {
+	// 		autstr = "BLUE GOALS ELIMS";
+	// 		con.print(0,0, "Aut 6: %s", autstr);
+	// 	}
+	// 	else if (atn ==7) {
+	// 		autstr = "SKILLS";
+	// 		con.print(0,0, "Aut 7: %s", autstr);
+	// 	}
+
+	// 	if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_X)){
+	// 		break;
+	// 	}
+}
+
+
+void opcontrol() {
+
+
+bool arcToggle = true;
+bool tankToggle=false;
+bool StakeWingToggle=false;
+double liftAngle=true;
+int time =0;
+bool hooks_Macro = false;
+bool  hooks_Macro_Rev = false;
+bool fishy_macro = false;
+bool return_fishmech = false;
+bool MogoMechToggle = false;
+bool LBC = false;
+int Macro = 0;
+bool IntakePiston = false;
+eyes.set_led_pwm(100);
+
+
+while (true) {
+	// delay(4000);
+	// Standard_AWP_blue();
+	// break;
+
+	//ColorSort();
+	//delay(10);
+	// if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
+	// 	//driveStraightC(250);
+	// 	// Intake.move(127);
+	// 	// driveSortHoldRed(1000, 50);
+	// 	//driveStraight2(500);
+	// 	MogoMech.set_value(true);
+	// }
+
+//Stakewing toggle
+ if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
+	//Intake.move(127);
+	// eyes.set_led_pwm(100);
+	// color = 2;
+	// while(true){
+	// 	ColorSort();
+	// 	delay(10);
+	// }
+	// driveSortHoldRed(750, 50);
+ }
+
+  
+
+  if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_Y)) {
+	StakeWingToggle = !StakeWingToggle;
+	 }
+StakeWing.set_value(StakeWingToggle);
+
+//Intake
+if (con.get_digital(E_CONTROLLER_DIGITAL_R1)){
+	Intake.move(127);
+	Intake.tare_position();
+	hooks_Macro = false; 
+}
+else if (con.get_digital(E_CONTROLLER_DIGITAL_R2)){
+	Intake.move (-127);
+	Intake.tare_position(); 
+	hooks_Macro = false; 
+} else {
+	Intake.move(0);
+	Intake.tare_position(); 
+	hooks_Macro = false; 
+}
+
+
+//lady brown macro 
+if(con.get_digital(E_CONTROLLER_DIGITAL_L1)){
+	LBC = false;
+	LadyBrown.move(127);
+	
+	
+} else if(con.get_digital(E_CONTROLLER_DIGITAL_L2)){
+	LBC = false;
+	LadyBrown.move(-127);
+
+	
+	
+ } else if (LBC == false){
+ 	LadyBrown.move(0);
+	 LadyBrown.brake();
+ }
+
+
+
+if (con.get_digital(E_CONTROLLER_DIGITAL_UP)) { //lady brown loading macro
+  // Set it into Macro Mode
+  LBC = true;
+  Macro++;
+
+  // Finds the angle that we need to move 
+  double current_angle = roto.get_angle() / 100.0;
+  double target_angle = 247;
+  double change_angle = target_angle - current_angle;
+  
+  // Finding how far to move the motor
+  double motor_ticks = change_angle * 5;
+
+  // Move motor to the desired position
+  LadyBrown.move_relative(motor_ticks, 90);
+} 
+
+if (con.get_digital(E_CONTROLLER_DIGITAL_DOWN)) { //lady brown descore macro
+	// Set it into Macro Mode
+	LBC = true;
+	Macro++;
+  
+	// Finds the angle that we need to move 
+	double current_angle = roto.get_angle() / 100.0;
+	double target_angle = 130;
+	double change_angle = target_angle - current_angle;
+	
+	// Finding how far to move the motor
+	double motor_ticks = change_angle * 5;
+  
+	// Move motor to the desired position
+	LadyBrown.move_relative(motor_ticks, 90);
+  } 
+  
+  if (con.get_digital(E_CONTROLLER_DIGITAL_LEFT)) { //lady brown double loading macro
+	// Set it into Macro Mode
+	LBC = true;
+	Macro++;
+  
+	// Finds the angle that we need to move 
+	double current_angle = roto.get_angle() / 100.0;
+	double target_angle = 220;
+	double change_angle = target_angle - current_angle;
+	
+	// Finding how far to move the motor
+	double motor_ticks = change_angle * 5;
+  
+	// Move motor to the desired position
+	LadyBrown.move_relative(motor_ticks, 90);
+  } 
+
+  if (con.get_digital(E_CONTROLLER_DIGITAL_RIGHT)) { //lady brown double loading macro
+	// Set it into Macro Mode
+	LBC = true;
+	Macro++;
+  
+	// Finds the angle that we need to move 
+	double current_angle = roto.get_angle() / 100.0;
+	double target_angle = 105;
+	double change_angle = target_angle - current_angle;
+	
+	// Finding how far to move the motor
+	double motor_ticks = change_angle * 5;
+  
+	// Move motor to the desired position
+	LadyBrown.move_relative(motor_ticks, 90);
+  }
+// if(LBC){
+// 	//LadyBrown.move(127);
+// 	setConstants(MACRO_KP, MACRO_KI, MACRO_KD);
+// 	Macro = 0;
+// 	if (Macro == 0){
+// 		LadyBrown.move(calcPID(35000, roto.get_angle(), 0, 0));//need to get the right angle optimal angle `
+// 	} else if (Macro == 1){
+// 		LadyBrown.move(calcPID(31000, roto.get_angle(), 0, 0));
+// 	}else {
+// 		//LadyBrown.move(0);
+		
+// 	}
+// }
+// make conversion variable
+
+
+//pistons for mogo
+
+if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_X)){
+MogoMechToggle = !MogoMechToggle;
+}  MogoMech.set_value(MogoMechToggle);
+
+//chassis drive 
+int power = con.get_analog(ANALOG_LEFT_Y);
+int RX = con.get_analog(ANALOG_RIGHT_X);
+int turn = int(pow(RX, 3)/ 16129);
+int left= power + turn; 
+int right = power - turn;
+
+// if(con.get_digital_new_press(E_CONTROLLER_DIGITAL_B)){
+// 	arcToggle = !arcToggle;
+// 	tankToggle = !tankToggle;
+// }
+
+if (tankToggle){
+	LF.move(con.get_analog(ANALOG_LEFT_Y));
+	LM.move(con.get_analog(ANALOG_LEFT_Y));
+	LB.move(con.get_analog(ANALOG_LEFT_Y));
+	RF.move(con.get_analog(ANALOG_RIGHT_Y));
+	RM.move(con.get_analog(ANALOG_RIGHT_Y));
+	RB.move(con.get_analog(ANALOG_RIGHT_Y));
+}
+
+if (arcToggle) {
+LF.move(left);
+LM.move(left);
+LB.move(left);
+RF.move(right);
+RM.move(right);
+RB.move(right);
+}
+double  chasstemp = ((RF.get_temperature() + RB.get_temperature() + LF.get_temperature() + LB.get_temperature())/4);
+if (time % 50 == 0 && time % 100 !=0 && time % 150 !=0){
+    con.print(0,0,"Time:%f       ", float(time2));//viewTime
+} else if (time% 100 == 0 && time % 150 !=0){
+    con.print(1,0,"HeadingError!%f      ", float(Macro));
+} else if (time % 150 == 0){
+    con.print(2,0,"C:%i H:%i LB:%i      ",int(chasstemp), int(Intake.get_temperature()), int(LadyBrown.get_temperature()));
+}
+
+delay(10);
+time += 10;
+
+}
+
+}
